3b6dad477033502c61d27190ce65d332b6f481a9
chuigda <icey@icey.tech>
2021-10-20 14:08:13 +0800

    掌柜的小破酒馆被人拆干净了后来 有人说那夜他被揍到走路一瘸一拐

--- src/unchecked.rs
+++ src/unchecked.rs
@@ -109,6 +109,44 @@ impl<T> UncheckedOption<T> {
     }
 }
 
+use std::cell::UnsafeCell;
+
+/// Unchecked operations added to `UnsafeCell`
+pub trait UncheckedCellOps {
+    type Target;
+
+    /// Assume the Rust aliasing model invariants are hold, gets an immutable reference from given
+    /// `UnsafeCell` without checking.
+    ///
+    /// # Safety
+    ///
+    /// If another mutable reference already exists, calling this function would immediately trigger
+    /// undefined behavior.
+    unsafe fn get_ref_unchecked(&self) -> &Self::Target;
+
+    /// Assume the Rust aliasing model invariants are hold, gets a mutable reference from given
+    /// `UnsafeCell` without checking.
+    ///
+    /// # Safety
+    ///
+    /// If another mutable reference or immutable reference already exists, calling this function
+    /// would immediately trigger undefined behavior.
+    unsafe fn get_mut_ref_unchecked(&self) -> &mut Self::Target;
+}
+
+impl<T> UncheckedCellOps for UnsafeCell<T> {
+    type Target = T;
+
+    unsafe fn get_ref_unchecked(&self) -> &Self::Target {
+        &*(self.get() as *const T)
+    }
+
+    unsafe fn get_mut_ref_unchecked(&self) -> &mut Self::Target {
+        &mut *self.get()
+    }
+}
+
+
 pub trait UnsafeFrom<T> {
     unsafe fn unsafe_from(data: T) -> Self;
 }
